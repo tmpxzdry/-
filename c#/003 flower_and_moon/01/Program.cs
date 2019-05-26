458a279e5318acbae2fc72fd8335bb0dc90b4c18
xzdry1 <289331962@qq.com>
2019-05-26 22:09:04 +0800

    add 003 flower and moon

---
+++ c#/003 flower_and_moon/01/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01
+{
+    public class Solution{
+        private IList<string> result;
+
+        public IList<string> Permute(int n){
+            result=new List<string>();
+            DFS(n,1,0,"女");
+            DFS(n,0,1,"男");
+            return result;
+        }
+
+        private void DFS(int n,int fc,int mc,string solution){
+            if(fc==n&&mc==n){
+                result.Add(solution);
+                return;
+            }
+            if(fc<n) DFS(n,fc+1,mc,solution+"女");
+            if(mc<n) DFS(n,fc,mc+1,solution+"男");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution=new Solution();
+            var result=solution.Permute(3);
+            foreach(var str in result){
+                System.Console.WriteLine(str);
+            }
+        }
+    }
+}
