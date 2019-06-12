a49888b63b7a83605dda01d0669fb0d4152541d0
xzdry1 <289331962@qq.com>
2019-06-12 22:24:20 +0800

    完成汉诺塔练习第一遍

---
+++ My/001 Hanoi/01/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi h = new Hanoi();
+            string A = "A";
+            string B = "B";
+            string C = "C";
+            long result = h.Process(3, A, B, C);
+            System.Console.WriteLine(result);
+        }
+    }
+
+    class Hanoi
+    {
+        private int count = 0;
+
+        public long Process(int n, string X, string Y, string Z)
+        {
+            if (n == 1)
+            {
+                count++;
+                System.Console.WriteLine("{0}.{1}->{2}", count, X, Z);
+            }
+            else
+            {
+                Process(n - 1, X, Z, Y);
+                count++;
+                System.Console.WriteLine("{0}.{1}->{2}", count, X, Z);
+                Process(n - 1, Y, X, Z);
+            }
+            return count;
+        }
+    }
+}
