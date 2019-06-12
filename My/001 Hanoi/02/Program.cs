774ece418bde866b9ff8346d3df3c35900564a72
xzdry1 <289331962@qq.com>
2019-06-12 23:53:58 +0800

    完成C#练习之汉诺塔第二遍（自己写）

---
+++ My/001 Hanoi/02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi h=new Hanoi();
+            string X="A";
+            string Y="B";
+            string Z="C";
+            h.Process(4,X,Y,Z);
+        }
+    }
+
+    class Hanoi{
+        private int count=0;
+
+        public long Process(int n,string X,string Y,string Z){
+            if(n==1){
+                count++;
+                System.Console.WriteLine("{0}.{1}->{2}",count,X,Z);
+            }else{
+                Process(n-1,X,Z,Y);
+                count++;
+                System.Console.WriteLine("{0}.{1}->{2}",count,X,Z);
+                Process(n-1,Y,X,Z);
+            }
+            return count;
+        }
+    }
+}
