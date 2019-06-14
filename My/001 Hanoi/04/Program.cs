b75c2a419deb55f79452e4245d2d289c35a28679
xzdry1 <289331962@qq.com>
2019-06-14 23:58:23 +0800

    try another way

---
+++ My/001 Hanoi/04/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04
+{
+    class Program
+    {
+        private int count=0;
+        static void Main(string[] args)
+        {
+            string X="A";
+            string Y="B";
+            string Z="C";
+            long result=Process(3,X,Y,Z);
+            System.Console.WriteLine(result);
+        }
+
+        public long Process(int n, string A,string B,string C){
+            if(n==1){
+                count++;
+                System.Console.WriteLine("{0} {1}->{2}",count,X,Z);
+            }else{
+                Process(n-1,X,Z,Y);
+                System.Console.WriteLine("{0} {1}->{2}",count,X,Z);
+                Process(n-1,Y,X,Z);
+            }
+            return count;
+        }
+    }
+}
