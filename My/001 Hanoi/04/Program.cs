af4d6ddd4cdfd4ffa4ff0933787a06e468fb29d5
xzdry1 <289331962@qq.com>
2019-06-15 00:03:23 +0800

    bug fix

--- My/001 Hanoi/04/Program.cs
+++ My/001 Hanoi/04/Program.cs
@@ -4,7 +4,7 @@ namespace _04
 {
     class Program
     {
-        private int count=0;
+        static int count=0;
         static void Main(string[] args)
         {
             string X="A";
@@ -14,12 +14,13 @@ namespace _04
             System.Console.WriteLine(result);
         }
 
-        public long Process(int n, string A,string B,string C){
+        static long Process(int n, string X,string Y,string Z){
             if(n==1){
                 count++;
                 System.Console.WriteLine("{0} {1}->{2}",count,X,Z);
             }else{
                 Process(n-1,X,Z,Y);
+                count++;
                 System.Console.WriteLine("{0} {1}->{2}",count,X,Z);
                 Process(n-1,Y,X,Z);
             }
